afd83ce08369e5fadd3b2a79b3029968641ba69b
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-01 19:34:19 +0530

    C a Pattern6.java

---
+++ Assignment1/Patterns Ques/Pattern6.java
@@ -0,0 +1,19 @@
+/*
+C  a
+C  b
+C  c
+*/
+import java.util.*;
+public class Pattern6
+{
+	public static void main(String[] args) {
+
+		for(char C='B';C<='B';C++)
+		{
+			for(char s='a';s<='c';s++)
+				System.out.println(C+" "+s);
+		}
+
+	}
+
+}
