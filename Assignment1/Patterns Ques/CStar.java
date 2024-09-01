ae5112c519cf627bfe750e14c0df365d676cb882
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-01 20:04:35 +0530

    CStar.java
    combination of 2 pervious prog

---
+++ Assignment1/Patterns Ques/CStar.java
@@ -0,0 +1,48 @@
+/*
+    * 
+   * * 
+  * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+*/
+
+
+import java.util.*;
+public class CStar
+{
+	public static void main(String[] args) {
+		Scanner star=new Scanner(System.in);
+		int n=star.nextInt();
+
+		for(int i=0; i<n; i++) //forrow
+		{
+			for (int j=0; j<n-i-1; j++) { //for space in column
+				System.out.print(" ");
+			}
+			for (int j=0; j<2*i+1; j++) { //for star in column
+				System.out.print("*");
+			}
+			for (int j=0; j<n-i-1; j++) { //for space in column
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+		for(int i=n-1; i>=0; i--) //forrow
+		{
+			for (int j=0; j<n-i-1; j++) { //for space in column
+				System.out.print(" ");
+			}
+			for (int j=0; j<2*i+1; j++) { //for star in column
+				System.out.print("*");
+			}
+			for (int j=0; j<n-i-1; j++) { //for space in column
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
