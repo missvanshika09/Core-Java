991b00532424d9cd067684e8f5a67360b19098ce
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-01 19:32:27 +0530

    B a Pattern5.java

---
+++ Assignment1/Patterns Ques/Pattern5.java
@@ -0,0 +1,19 @@
+/*B  a
+B  b
+B  c
+
+  */
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+
+		for(char C='B';C<='B';C++)
+		{
+			for(char s='a';s<='c';s++)
+				System.out.println(C+" "+s);
+		}
+
+	}
+
+}
