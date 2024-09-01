ddabba45040d5a6148a6ffcfb59f1ab5433aacb6
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-01 12:47:31 +0530

    First Pattern Program

---
+++ Assignment1/Program1/Main.java
@@ -0,0 +1,16 @@
+/*Pattern to be Printed :
+    A a
+    A b
+    A c
+*/
+
+
+public class Main {
+  public static void main(String args[]) {
+    for(int i=0;i<3;i++){
+        char y = (char)(97+i);
+        System.out.println("A "+y);
+    }
+    
+  }
+}
