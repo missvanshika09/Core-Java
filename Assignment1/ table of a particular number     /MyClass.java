c57458035585c9b7e3aacdc643f262280488c6bf
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-01 15:33:30 +0530

    MyClass.java
    /*display a table of a particular number

---
+++ Assignment1/ table of a particular number     /MyClass.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MyClass
+{
+	public static void main(String[] args) {
+		Scanner sum= new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		int x,y;
+		x=sum.nextInt();
+		y=sum.nextInt();
+
+		System.out.println(x+y) ;
+
+	}
+}
