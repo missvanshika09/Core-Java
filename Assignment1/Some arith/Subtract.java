22a3d154353928477bdc419e23979aeec996d149
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-02 17:13:19 +0530

    Create Subtract.java
     accept two numbers and subtract them.

---
+++ Assignment1/Some arith/Subtract.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Subtract 
+{	//accept two numbers and add them.
+	public static void main(String[] args) {
+		Scanner sub=new Scanner(System.in);
+		int x=sub.nextInt();
+		int y=sub.nextInt();
+		System.out.println(x-y);
+	}
+}
