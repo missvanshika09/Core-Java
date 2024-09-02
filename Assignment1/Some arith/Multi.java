200f7bc59d41224c0a268f6756aaf65bf8633a72
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-02 17:14:59 +0530

    Multi.java
     accept two numbers and multiply them.

---
+++ Assignment1/Some arith/Multi.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Multi 
+{	//accept two numbers and add them.
+	public static void main(String[] args) {
+		Scanner multi =new Scanner(System.in);
+		int x=multi.nextInt();
+		int y=multi.nextInt();
+		System.out.println(x*y);
+	}
+}
