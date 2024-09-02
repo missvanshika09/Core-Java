658426da106128b34a89c2c89d35031a0561a2dc
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-02 17:17:11 +0530

    Divi.java

---
+++ Assignment1/Some arith/Divi.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Divi 
+{	//accept two numbers and divi them.
+	public static void main(String[] args) {
+		Scanner divi=new Scanner(System.in);
+		int x=divi.nextInt();
+		int y=divi.nextInt();
+		System.out.println(x/y);
+	}
+}
