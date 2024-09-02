738ff1ea1e16da690c89b10a3fa386367e51cd5c
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-02 17:10:26 +0530

    Addition.java
    accept two numbers and add them.

---
+++ Assignment1/Some arith/Addition.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Addition
+{	//accept two numbers and add them.
+	public static void main(String[] args) {
+		Scanner add=new Scanner(System.in);
+		int x=add.nextInt();
+		int y=add.nextInt();
+		System.out.println(x+y);
+	}
+}
