636585d826b4cba0f75c505ddb5136154f47f86a
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-04 18:46:57 +0530

    Arr_9.java

---
+++ Assignment3/Arr_9.java
@@ -0,0 +1,33 @@
+//. create int array of 5 elements and accept 5 values inside it. Now accept one more number and check whether that number is there inside array or not.
+package Vanshika;
+import java.util.*;
+public class Arr_9 {
+
+	public static void main(String[] args) {
+
+		int a[];
+		Scanner sc = new Scanner(System.in);
+		a=new int[5];
+		System.out.println("Enter the elements of array");
+		for(int i=0;i<a.length;i++) {
+			a[i]=sc.nextInt();	
+			
+		}
+		System.out.println("Enter a number to be searched for ");
+		int n=sc.nextInt();
+		boolean found=false;
+		for(int i=0;i<a.length;i++) {
+			if(a[i]==n) {
+				System.out.println(" Number "+n+" found at index "+i);
+				found = true;
+				break;
+			}
+			if (!found) {
+	            System.out.println("Number " + n + " not found in the array.");
+	        }
+		}
+		sc.close();
+}
+
+}
+
