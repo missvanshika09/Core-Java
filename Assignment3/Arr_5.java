35562b1508055cf55b96ae37dad4fcc44834a680
3or4 <160387685+missvanshika09@users.noreply.github.com>
2024-09-04 18:38:37 +0530

    Arr_5.java
    create int array of 5 elements and accept 5 values inside it. Now display all the numbers of array.

---
+++ Assignment3/Arr_5.java
@@ -0,0 +1,16 @@
+package Vanshika;
+import java.util.*;
+public class Arr_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[];
+		Scanner sc = new Scanner(System.in);
+		a=new int[5];
+		for(int i=0;i<=a.length;i++) {
+			a[i]=sc.nextInt();
+			System.out.println(a[i]);
+			
+		}
+		sc.close();
+		
